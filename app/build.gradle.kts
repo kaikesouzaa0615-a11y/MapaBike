2a7a1310b2b27e66f305a449f90a24ca35128539
kaikesouzaa0615-a11y <kaikesouzaa0615@gmail.com>
2026-09-19 02:56:34 +0000

    Fix Java Kotlin JVM target

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
 }
 
 android {
+    compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
+    kotlinOptions { jvmTarget = "17" }
     kotlinOptions { jvmTarget = "17" }
     compileOptions {
         sourceCompatibility JavaVersion.VERSION_17
