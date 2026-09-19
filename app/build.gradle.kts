af81507f2cd3c6fe7952e2c7a082ce423b9473ca
kaikesouzaa0615-a11y <kaikesouzaa0615@gmail.com>
2026-09-19 03:15:16 +0000

    Corrigir Java 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,6 +13,8 @@ android {
     }
     namespace = "com.example.mapabike"
     compileSdk = 35
+    compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
+    kotlinOptions { jvmTarget = "17" }
 
     defaultConfig {
         applicationId = "com.example.mapabike"
